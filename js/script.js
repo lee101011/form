b55f5153eebe64ed340bbbc3cde3ea2a5ed1b02c
Your Name <you@example.com>
2024-04-19 17:13:46 -0700

    Add better password validation

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const pass2 = document.querySelector("#form-password2");
 const passMessage = document.querySelector(".passcheck");
 
 function validPasswords() {
-  return pass.value === pass2.value && pass.value.length > 0;
+  return (pass.value === pass2.value) && (pass.value.length > 0
+    || pass2.value.length > 0);
 }
 
-pass2.addEventListener("change",(event) => {
-  if(validPasswords()) {
+function validatePassword(event) {
+  console.log("validating from", event)
+  if (validPasswords()) {
     passMessage.textContent = "";
   } else {
     passMessage.textContent = "* Passwords do not match";
   }
-});
+}
+
+pass.addEventListener("keyup", validatePassword);
+pass.addEventListener("focusout", validatePassword);
+pass2.addEventListener("keyup", validatePassword);
+pass2.addEventListener("focusout", validatePassword);
